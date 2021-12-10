0a0d0b40dd11347102a48ba5ef13a527f14e9efa
FLORIAN DEMOUSTIER <florian.demoustier@gmail.com>
2021-12-10 11:12:20 +0100

    Exercice 4

--- Evaluation/src/app/app.component.ts
+++ Evaluation/src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { Statistique } from './models/statistique';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'Evaluation';
+  vaccination = new Statistique('Nombre de personne vaccine', 2021, 80);
+  bitcoin = new Statistique('Augmentation cours Bitcoin', 2021, 15);
 }
--- Evaluation/src/app/models/statistique.ts
+++ Evaluation/src/app/models/statistique.ts
@@ -1,2 +1,10 @@
 export class Statistique {
+  nom: string;
+  annee: number;
+  taux: number;
+  constructor(nom: string, taux: number, annee: number) {
+    this.nom = nom;
+    this.annee = annee;
+    this.taux = taux;
+  }
 }
