4870d7e5d3054002807f3d0b200ae35a0b149de4
FLORIAN DEMOUSTIER <florian.demoustier@gmail.com>
2021-12-10 12:39:07 +0100

    Exercice 8

--- Evaluation/src/app/app.component.ts
+++ Evaluation/src/app/app.component.ts
@@ -8,10 +8,14 @@ import { Statistique } from './models/statistique';
 })
 export class AppComponent {
   tabStat: Statistique[] = [];
-  // vaccination = new Statistique('Nombre de personne vaccine', 2021, 80);
-  // bitcoin = new Statistique('Augmentation cours Bitcoin', 2021, 15);
+  vaccination = new Statistique('Nombre de personne vaccine', 2021, 80);
+  bitcoin = new Statistique('Augmentation cours Bitcoin', 2021, 15);
 
   constructor() {
-    // this.tabStat.push(this.vaccination, this.bitcoin);
+    this.tabStat.push(this.vaccination, this.bitcoin);
+  }
+  supprimer(uneStat: Statistique) {
+    let index = this.tabStat.indexOf(uneStat);
+    this.tabStat.splice(index, 1);
   }
 }
